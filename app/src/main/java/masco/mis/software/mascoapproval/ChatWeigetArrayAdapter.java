70315ad017545d51d632118a2e4b3ffa792ea3ba
xautopilot <xautopilotwhite@gmail.com>
2016-12-19 10:00:46 +0600

    chat list scroll listener added

--- app/src/main/java/masco/mis/software/mascoapproval/ChatWeigetArrayAdapter.java
+++ app/src/main/java/masco/mis/software/mascoapproval/ChatWeigetArrayAdapter.java
@@ -1,11 +1,18 @@
 package masco.mis.software.mascoapproval;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,40 +25,98 @@ import masco.mis.software.mascoapproval.pojo.ChatWeiget;
 public class ChatWeigetArrayAdapter extends ArrayAdapter<ChatWeiget> {
 
     private Context _context;
-    int _layoutId;
+    private int _layoutId;
+    List<ChatWeiget> _items;
 
-    public ChatWeigetArrayAdapter(Context context,
-                            int layoutId,
-                            List<ChatWeiget> items) {
+    public ChatWeigetArrayAdapter(Context context, int layoutId, List<ChatWeiget> items) {
         super(context, layoutId, items);
         _context = context;
         _layoutId = layoutId;
+        _items=items;
     }
 
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-
+        // Get the data item for this position
         ChatWeiget chatWeigetItem = getItem(position);
 
+        ViewHolder viewHolder; // view lookup cache stored in tag
 
+        // Check if an existing view is being reused, otherwise inflate the view
         if (view == null) {
-            LayoutInflater inflater = (LayoutInflater)_context.
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) _context.
                     getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(_layoutId, parent,false);
+            view = inflater.inflate(_layoutId, parent, false);
+            //another way of doing this
+            //view = LayoutInflater.from(getContext()).inflate(_layoutId, parent, false);
+
+            viewHolder.txtTop = (TextView) view.findViewById(R.id.txt_chatweiget_top);
+            viewHolder.txtMiddle = (TextView) view.findViewById(R.id.txt_chatweiget_middle);
+            viewHolder.txtBottom = (TextView) view.findViewById(R.id.txt_chatweiget_bottom);
+            viewHolder.txtTopRight = (TextView) view.findViewById(R.id.txt_chatweiget_topRight);
+            viewHolder.imgEmp = (ImageView) view.findViewById(R.id.img_home_employee);
+
+
+
+            viewHolder.imgEmp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    ListView mListView = (ListView) view.getParent().getParent();
+                    final int position = mListView.getPositionForView((View) view.getParent());
+                    Toast.makeText(_context, "Test :" + _items.get(position).emp_code, Toast.LENGTH_SHORT).show();
+
+                    /*final Dialog dialog = new Dialog(_context);
+                    dialog.setContentView(R.layout.dialog_forward_to);
+                    dialog.setTitle("EMP Info.....");
+                    ((Button) dialog.findViewById(R.id.btn_dialog_cancel)).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();*/
+
+                    //Log.v("arman",String.valueOf(position));
+
+
+                }
+            });
+
+            /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                    Log.v("arman",String.valueOf(l));
+                }
+            });*/
+
+
+
+            // Cache the viewHolder object inside the fresh view
+            view.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) view.getTag();
         }
 
-        TextView chatweiget_top = (TextView)view.findViewById(R.id.txt_chatweiget_top);
-        TextView chatweiget_middle = (TextView)view.findViewById(R.id.txt_chatweiget_middle);
-        TextView chatweiget_bottom = (TextView)view.findViewById(R.id.txt_chatweiget_bottom);
-        TextView chatweiget_topRight = (TextView)view.findViewById(R.id.txt_chatweiget_topRight);
-
-
-        chatweiget_top.setText(chatWeigetItem.txtTop);
-        chatweiget_middle.setText(chatWeigetItem.txtMiddle);
-        chatweiget_bottom.setText(chatWeigetItem.txtBottom);
-        chatweiget_topRight.setText(chatWeigetItem.txtTopRight);
+        viewHolder.txtTop.setText(chatWeigetItem.txtTop);
+        viewHolder.txtMiddle.setText(chatWeigetItem.txtMiddle);
+        viewHolder.txtBottom.setText(chatWeigetItem.txtBottom);
+        viewHolder.txtTopRight.setText(chatWeigetItem.txtTopRight);
 
         return view;
+    }
+
+    private static class ViewHolder {
+        protected TextView txtTop;
+        protected TextView txtMiddle;
+        protected TextView txtBottom;
+        protected TextView txtTopRight;
+        protected ImageView imgEmp;
+
     }
 }
---
+++ app/src/main/java/masco/mis/software/mascoapproval/chat/pojo/ChatMessage.java
@@ -0,0 +1,31 @@
+package masco.mis.software.mascoapproval.chat.pojo;
+
+import java.util.Random;
+
+/**
+ * Created by ARMAN on 14-Dec-16.
+ */
+
+public class ChatMessage {
+
+    public String body, sender, receiver, senderName;
+    public String Date, Time;
+    public String msgid;
+    public boolean isMine;// Did I send the message.
+
+    public ChatMessage(String Sender, String Receiver, String messageString,
+                       String ID, boolean isMINE) {
+        body = messageString;
+        isMine = isMINE;
+        sender = Sender;
+        msgid = ID;
+        receiver = Receiver;
+        senderName = sender;
+    }
+
+    public void setMsgID() {
+
+        msgid += "-" + String.format("%02d", new Random().nextInt(100));
+
+    }
+}
