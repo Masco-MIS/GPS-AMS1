ebe944560825fd93316646d4e239510414d66b46
Md.Tahmid Hossain <mdtahmidhossain@gmail.com>
2016-12-20 11:18:50 +0600

    SP updated

--- app/src/main/java/masco/mis/software/mascoapproval/auxiliary/StoredProcedure.java
+++ app/src/main/java/masco/mis/software/mascoapproval/auxiliary/StoredProcedure.java
@@ -7,4 +7,5 @@ package masco.mis.software.mascoapproval.auxiliary;
 public class StoredProcedure {
     public static  String sp_get_emp_list = "usp_M_get_emp_for_auto";
     public static String forward_approval ="sp_M_insert_forward";
+    public static String get_po = "usp_m_get_po";
 }
